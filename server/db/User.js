bbc6be1fed7f76bd4a932422bddb31d4fa111dfc
Richard Tang <richardtang27@gmail.com>
2023-05-18 19:28:15 -0700

    setup

--- server/db/User.js
+++ server/db/User.js
@@ -4,69 +4,89 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const JWT = process.env.JWT;
 
-
 const User = conn.define('user', {
   id: {
     type: UUID,
     primaryKey: true,
-    defaultValue: UUIDV4
+    defaultValue: UUIDV4,
   },
   username: {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
+      notEmpty: true,
     },
-    unique: true
+    unique: true,
   },
   password: {
     type: STRING,
     allowNull: false,
     validate: {
-      notEmpty: true
-    }
-  }
+      notEmpty: true,
+    },
+  },
+  firstName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+    unique: true,
+  },
 });
 
-User.addHook('beforeSave', async(user)=> {
-  if(user.changed('password')){
+User.addHook('beforeSave', async (user) => {
+  if (user.changed('password')) {
     user.password = await bcrypt.hash(user.password, 5);
   }
 });
 
-User.findByToken = async function(token){
+User.findByToken = async function (token) {
   try {
     const { id } = jwt.verify(token, process.env.JWT);
     const user = await this.findByPk(id);
-    if(user){
+    if (user) {
       return user;
     }
     throw 'user not found';
-  }
-  catch(ex){
+  } catch (ex) {
     const error = new Error('bad credentials');
     error.status = 401;
     throw error;
   }
-}
+};
 
-User.prototype.generateToken = function(){
+User.prototype.generateToken = function () {
   return jwt.sign({ id: this.id }, JWT);
 };
 
-User.authenticate = async function({ username, password }){
+User.authenticate = async function ({ username, password }) {
   const user = await this.findOne({
     where: {
-      username
-    }
+      username,
+    },
   });
-  if(user && await bcrypt.compare(password, user.password)){
+  if (user && (await bcrypt.compare(password, user.password))) {
     return jwt.sign({ id: user.id }, JWT);
   }
   const error = new Error('bad credentials');
   error.status = 401;
   throw error;
-}
+};
 
 module.exports = User;
-
