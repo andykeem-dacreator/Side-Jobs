0b6f58fb8571cbd8187ea95bf0bc71ba299794dc
Andy Kim <youngkim14@gmail.com>
2023-05-18 22:27:21 -0400

    task model

---
+++ server/db/Task.js
@@ -0,0 +1,33 @@
+const conn = require('./conn');
+const { STRING, TEXT, UUID, UUIDV4, FLOAT } = conn.Sequelize;
+
+const Task = conn.define('task', {
+  id: {
+    type: UUID,
+    primaryKey: true,
+    defaultValue: UUIDV4
+  },
+  title: {
+    type: TEXT,
+  },
+  description: {
+    type: TEXT,
+  },
+  price: {
+    type: FLOAT,
+  },
+  city: {
+    type: TEXT,
+  },
+  state: {
+    type: TEXT,
+  },
+  category: {
+    type: ENUM('virtual', 'shopping', 'misc', 'moving', 'sport', 'gaming', 'photography'),
+  },
+  taskDoerId: {
+    type: STRING
+  }
+});
+
+module.exports = Task;
