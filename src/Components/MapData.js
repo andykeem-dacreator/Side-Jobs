969e70673d856dcd1dca6f3eb962f22a06ae0331
Andy Kim <youngkim14@gmail.com>
2024-01-12 15:45:38 -0500

    added new api key

--- src/Components/MapData.js
+++ src/Components/MapData.js
@@ -1,23 +1,23 @@
-import React, { useState, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React, { useState, useMemo } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
-} from '@react-google-maps/api';
+} from "@react-google-maps/api";
 const MapData = () => {
   const { tasks } = useSelector((state) => state);
   const { id } = useParams();
-  // const [activeMarker, setActiveMarker] = useState(null);
+  const [activeMarker, setActiveMarker] = useState(null);
   const task = tasks.find((task) => task.id === id);
-  // const { isLoaded } = useLoadScript({
-  //     googleMapsApiKey: process.env.REACT_APP_API_KEY,
-  // });
-  // const handleMarkerClick = (marker) => {
-  //     setActiveMarker(marker);
-  // };
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+  const handleMarkerClick = (marker) => {
+    setActiveMarker(marker);
+  };
   const center = useMemo(
     () => ({ lat: parseFloat(task.lat), lng: parseFloat(task.lng) }),
     []
@@ -26,9 +26,9 @@ const MapData = () => {
   if (!task) {
     return null;
   }
-  // if (!isLoaded) {
-  //     return <div>Loading...</div>;
-  // }
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
   return (
     <GoogleMap zoom={12} center={center} mapContainerClassName="map-container">
       <Marker position={center} />
