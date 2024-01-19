33ec25bb11e7364f8e4eca78d6057f984b31254f
Andy Kim <youngkim14@gmail.com>
2024-01-19 16:22:26 -0500

    attempt2

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,7 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { createTask } from '../store';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import { createTask } from "../store";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import {
   TextField,
   Button,
@@ -10,33 +10,33 @@ import {
   InputLabel,
   FormControl,
   InputAdornment,
-} from '@mui/material';
-import Typography from '@mui/material/Typography';
+} from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 const AddTask = () => {
   const { auth, users } = useSelector((state) => state);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [street, setStreet] = useState('');
-  const [city, setCity] = useState('');
-  const [state, setState] = useState('');
-  const [zipCode, setZipCode] = useState('');
-  const [country, setCountry] = useState('');
-  const [lat, setLat] = useState('');
-  const [lng, setLng] = useState('');
-  const [price, setPrice] = useState('');
-  const [category, setCategory] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [country, setCountry] = useState("");
+  const [lat, setLat] = useState("");
+  const [lng, setLng] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [error, setError] = useState(null);
 
   function isEnoughMoney(price) {
-    return auth.wallet > price  ;
+    return auth.wallet > price;
   }
 
   const handleMoney = (ev) => {
     if (!isEnoughMoney(ev.target.value)) {
-      setError('Insufficient Funds!!!');
+      setError("Insufficient Funds!!!");
     } else {
       setError(null);
     }
@@ -48,21 +48,21 @@ const AddTask = () => {
     if (input.current) {
       const autocomplete = new google.maps.places.Autocomplete(input.current, {
         fields: [
-          'address_components',
-          'geometry',
-          'icon',
-          'name',
-          'formatted_address',
+          "address_components",
+          "geometry",
+          "icon",
+          "name",
+          "formatted_address",
         ],
       });
-      autocomplete.addListener('place_changed', () => {
+      autocomplete.addListener("place_changed", () => {
         const place = autocomplete.getPlace();
         if (place.address_components) {
-          let Address = place.formatted_address.split(',');
+          let Address = place.formatted_address.split(",");
           setStreet(Address[0]);
           setCity(Address[1]);
-          setState(Address[2].split(' ')[1]);
-          setZipCode(Address[2].split(' ')[2]);
+          setState(Address[2].split(" ")[1]);
+          setZipCode(Address[2].split(" ")[2]);
           setCountry(Address[3]);
           setLat(place.geometry.location.lat);
           setLng(place.geometry.location.lng);
@@ -72,15 +72,15 @@ const AddTask = () => {
   }, [input]);
 
   const categories = [
-    'virtual',
-    'shopping',
-    'misc',
-    'moving',
-    'sport',
-    'gaming',
-    'photography',
-    'beauty',
-    'cleaning'
+    "virtual",
+    "shopping",
+    "misc",
+    "moving",
+    "sport",
+    "gaming",
+    "photography",
+    "beauty",
+    "cleaning",
   ];
   const create = async (ev) => {
     ev.preventDefault();
@@ -100,7 +100,7 @@ const AddTask = () => {
         userId: auth.id,
       })
     );
-    navigate('/myTasks');
+    navigate("/myTasks");
   };
 
   return (
@@ -135,12 +135,15 @@ const AddTask = () => {
           onChange={handleMoney}
           placeholder="Price (ex: 1000)"
           error={error && !isEnoughMoney(price)}
-          helperText={error && isNaN(price) ? 'Enter a Number' : error && !isEnoughMoney(price) ? 'Insufficient Funds!!!' : ''}
+          helperText={
+            error && isNaN(price)
+              ? "Enter a Number"
+              : error && !isEnoughMoney(price)
+              ? "Insufficient Funds!!!"
+              : ""
+          }
           InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">$
-                </InputAdornment>
-            ),
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
           }}
         />
         <FormControl>
@@ -163,7 +166,7 @@ const AddTask = () => {
         <Button
           type="submit"
           variant="contained"
-          disabled={!title || !description || !price || !category || !street}
+          disabled={!title || !description || !price || !category || !!street}
         >
           Post
         </Button>
--- src/Components/MapData.js
+++ src/Components/MapData.js
@@ -1,4 +1,47 @@
-import React, { useState, useMemo } from "react";
+// import React, { useState, useMemo } from "react";
+// import { useSelector } from "react-redux";
+// import { useParams } from "react-router-dom";
+// import {
+//   GoogleMap,
+//   useLoadScript,
+//   Marker,
+//   InfoWindow,
+// } from "@react-google-maps/api";
+
+// // require("dotenv").config();
+
+// const MapData = () => {
+//   const { tasks } = useSelector((state) => state);
+//   const { id } = useParams();
+//   const [activeMarker, setActiveMarker] = useState(null);
+//   const task = tasks.find((task) => task.id === id);
+//   const { isLoaded } = useLoadScript({
+//     apiKey: process.env.REACT_APP_API_KEY,
+//   });
+//   const handleMarkerClick = (marker) => {
+//     setActiveMarker(marker);
+//   };
+//   const center = useMemo(
+//     () => ({ lat: parseFloat(task.lat), lng: parseFloat(task.lng) }),
+//     []
+//   );
+
+//   if (!task) {
+//     return null;
+//   }
+//   if (!isLoaded) {
+//     return <div>Loading...</div>;
+//   }
+//   return (
+//     <GoogleMap zoom={12} center={center} mapContainerClassName="map-container">
+//       <Marker position={center} />
+//     </GoogleMap>
+//   );
+// };
+
+// export default MapData;
+
+import React, { useState, useMemo, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import {
@@ -8,35 +51,52 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 
-// require("dotenv").config();
-
 const MapData = () => {
   const { tasks } = useSelector((state) => state);
   const { id } = useParams();
   const [activeMarker, setActiveMarker] = useState(null);
   const task = tasks.find((task) => task.id === id);
-  const { isLoaded } = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     apiKey: process.env.REACT_APP_API_KEY,
   });
+  const [googleMap, setGoogleMap] = useState(null);
+
   const handleMarkerClick = (marker) => {
     setActiveMarker(marker);
   };
+
   const center = useMemo(
     () => ({ lat: parseFloat(task.lat), lng: parseFloat(task.lng) }),
     []
   );
 
+  useEffect(() => {
+    if (isLoaded) {
+      setGoogleMap(
+        <GoogleMap
+          zoom={12}
+          center={center}
+          mapContainerClassName="map-container"
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      );
+    }
+  }, [isLoaded, center]);
+
+  if (loadError) {
+    return <div>Error loading Google Maps</div>;
+  }
+
   if (!task) {
     return null;
   }
+
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
-  return (
-    <GoogleMap zoom={12} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
-  );
+
+  return googleMap;
 };
 
 export default MapData;
